3b9031af81bc2c7b4c9d019794881c836828eb80
Hayden <chaycover.ytb@gmail.com>
2026-07-01 11:55:39 +0200

    Fix library paywall message

--- public/app.js
+++ public/app.js
@@ -421,7 +421,7 @@ async function renderLibrary() {
       libraryNotice.classList.remove('hidden');
       document.getElementById('notice-upgrade')?.addEventListener('click', e => {
         e.preventDefault();
-        window.Auth?.showPaywall();
+        window.Auth?.showPaywall('upgrade');
       });
     }
   }
